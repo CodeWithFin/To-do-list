7d421053cd2ca774fb1d7bb58877598bc16ea6dc
Finley <Finley.mwachia12@gmail.com>
2024-08-29 13:48:44 +0300

    use ustate to manage items being entered in the array list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [listItem, setListItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setListItem(value);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, listItem];
+    });
+    setListItem(""); // Clear the input field after adding the item
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          type="text"
+          name="item"
+          onChange={handleChange}
+          value={listItem}
+        />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
